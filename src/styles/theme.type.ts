79677b4cdb5344c0d70fa92c56018984fa64d6e9
hailey-hy <lillyine.hy@gmail.com>
2025-04-28 16:05:01 +0900

    chore: theme 객체 자동완성되도록 타입 정리

---
+++ src/styles/theme.type.ts
@@ -0,0 +1,29 @@
+import { css } from "styled-components";
+import { colors, fonts } from "./theme";
+
+type CssReturnType = ReturnType<typeof css>;
+
+export type ThemeColors = typeof colors;
+
+export type ThemeFonts = {
+  [K in keyof typeof fonts]: CssReturnType;
+};
+
+export interface ThemeMixins {
+  flexBox: (
+    direction?: string,
+    justify?: string,
+    align?: string
+  ) => CssReturnType;
+  skeleton: () => CssReturnType;
+  slideUp: () => CssReturnType;
+  fadeIn: () => CssReturnType;
+  slideUpWFadeIn: () => CssReturnType;
+  gradientPrimary: () => CssReturnType;
+}
+
+export interface ThemeType {
+  colors: ThemeColors;
+  fonts: ThemeFonts;
+  mixins: ThemeMixins;
+}
