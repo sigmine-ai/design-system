a84f2a3007296402f374ddb909051756c1a74289
hailey-hy <lillyine.hy@gmail.com>
2025-05-01 13:33:58 +0900

    feat: theme export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as Icon } from "./components/icon/Icon";
 export { default as Text } from "./components/text/Text";
 export { default as Button } from "./components/button/Button";
 export { default as Input } from "./components/input/Input";
+export { default as theme } from "./styles/theme";
 export * from "./styles/theme.type";
