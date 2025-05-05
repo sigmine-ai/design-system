8d9795406e5311c88f2bd85fa2b2b3ee1d5dd78a
hailey-hy <lillyine.hy@gmail.com>
2025-05-05 15:18:57 +0900

    fix: textarea 내보내지 않는 오류 해결

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Icon } from "./components/icon/Icon";
 export { default as Text } from "./components/text/Text";
+export { default as Textarea } from "./components/textArea/Textarea";
 export { default as Button } from "./components/button/Button";
 export { default as Input } from "./components/input/Input";
 export { default as theme } from "./styles/theme";
